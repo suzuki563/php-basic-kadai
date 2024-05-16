680d1994db0abe08dddd8d20424e2cbad0514837
suzuki563 <135518774+suzuki563@users.noreply.github.com>
2024-05-16 15:56:46 +0900

    Add files via upload

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <title>課題7</title>
+</head>
+
+<body>
+    <p>
+        <?php
+
+        $vegetable = ['name' => 'onion', 'price' => 200, 'weight' => 160];
+
+        print_r($vegetable);
+
+        ?>
+    </p>
+
+
+
+</body>
+
+</html>
