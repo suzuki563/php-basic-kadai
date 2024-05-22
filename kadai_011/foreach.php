c7bff2a8a1ee3069243e04e227c9336691d541f5
suzuki563 <135518774+suzuki563@users.noreply.github.com>
2024-05-22 22:13:07 +0900

    Add files via upload

---
+++ kadai_011/foreach.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+ <html lang="ja">
+ 
+ <head>
+     <meta charset="UTF-8">
+     <title>kadai_11</title>
+ </head>
+ 
+ <body>
+ <p>
+    <?php
+    $vegetables_data = [ "名前" => "玉ねぎ",
+    "値段" => 200,
+    "産地" => "北海道"];
+
+    foreach($vegetables_data as $key =>$value ){
+      echo $key. ":".$value."<br>";
+    }
+
+    ?>
+ </p>
+ </body>
+ 
+ </html>
